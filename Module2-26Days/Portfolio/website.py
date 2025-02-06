4a6cf0e9182744c19d5c7eb78c38128d898b2a23
QA-RW-Automation <qarwautomation@gmail.com>
2025-02-06 14:00:14 +0530

    rendered the title

--- Module2-26Days/Portfolio/website.py
+++ Module2-26Days/Portfolio/website.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from dateutil.utils import within_delta
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -10,6 +12,26 @@ with col1:
 with col2:
     st.title("Kajal Chaudhary")
     content = """I am Kajal, a skilled software tester with 7 years of experience in ensuring high-quality software solutions. "
-               "My expertise lies in test automation, functional testing, and performance testing, ensuring seamless user experiences and robust application performance. With a keen eye for detail and a passion for quality assurance, I have successfully contributed to multiple projects across various domains."""
+               "My expertise lies in test automation, functional testing, and performance testing, 
+               ensuring seamless user experiences and robust application performance. 
+               With a keen eye for detail and a passion for quality assurance, 
+               I have successfully contributed to multiple projects across various domains."""
     st.info(content)
+
+content2 = """Below you can find some of the apps, I have built in python. Feel free to contact me."""
+
+st.write(content2  )
+
+col3, col4 = st.columns(2)
+
+df  = pandas.read_csv("data.csv", sep = ";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
 
